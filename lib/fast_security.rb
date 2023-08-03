de730a61837d836f79870c0a68b0ba6acad52892
Stephen MacVicar <smacvicar@mitre.org>
2023-08-03 09:41:28 -0400

    update description

--- lib/fast_security.rb
+++ lib/fast_security.rb
@@ -4,20 +4,26 @@ module FASTSecurity
   class Suite < Inferno::TestSuite
     id :fast_security
     title 'FAST Security'
-    description %( The FAST Security Suite verifies that systems correctly implement 
-      the [FAST Security IG](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/) 
-      for extending OAuth 2.0 using UDAP workflows. Currently, only the 
-      [discovery requirements](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/discovery.html) are
-      tested by the first test group, verifying the presence of metadata which 
-      represents server capabilities. The tested metadata includes all required, optional, recommended, and conditional
-      parameters. It does not, however, test for the support of the optional `community` parameter
-      to get a certificate intended for use within the identified trust community.
-      Complete testing of compliance with the FAST Security IG will require testing of 
-      [registration](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/registration.html),
-      authorization and authentication of [consumer-facing apps](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/consumer.html)
-      and [B2B apps](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/b2b.html), 
-      [tiered user authentication](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/user.html),
-      and additional conformance to [FHIR artifacts](https://build.fhir.org/ig/HL7/fhir-udap-security-ig/artifacts.html) (TBD). 
+    description %(
+      The FAST Security Suite verifies that systems correctly implement the
+      [FAST Security FHIR® IG](http://hl7.org/fhir/us/udap-security/STU1/)
+      for extending OAuth 2.0 using UDAP workflows. Currently, only the
+      [discovery
+      requirements](http://hl7.org/fhir/us/udap-security/STU1/discovery.html)
+      are tested, verifying the presence of metadata which represents server
+      capabilities. The tested metadata includes all required, optional,
+      recommended, and conditional parameters except for the optional
+      `community` parameter.
+
+      Complete testing of compliance with the FAST Security IG will require
+      testing of
+      [registration](http://hl7.org/fhir/us/udap-security/STU1/registration.html),
+      authorization and authentication of [consumer-facing
+      apps](http://hl7.org/fhir/us/udap-security/STU1/consumer.html)
+      and [B2B
+      apps](http://hl7.org/fhir/us/udap-security/STU1/b2b.html),
+      and [tiered OAuth for user
+      authentication](http://hl7.org/fhir/us/udap-security/STU1/user.html).
     )
 
     group from: :discovery_group
