6746350b5f9fe03630eba1c6c05ce89075def076
rpassas <35311744+rpassas@users.noreply.github.com>
2023-08-16 11:20:09 -0400

    suite description (#6)
    Co-authored-by: rpassas <rpassas@mitre.org>
    Co-authored-by: Stephen MacVicar <smacvicar@mitre.org>

--- lib/fast_security.rb
+++ lib/fast_security.rb
@@ -4,7 +4,27 @@ module FASTSecurity
   class Suite < Inferno::TestSuite
     id :fast_security
     title 'FAST Security'
-    description 'Tests for FAST Security'
+    description %(
+      The FAST Security Suite verifies that systems correctly implement the
+      [FAST Security FHIR® IG](http://hl7.org/fhir/us/udap-security/STU1/)
+      for extending OAuth 2.0 using UDAP workflows. Currently, only the
+      [discovery
+      requirements](http://hl7.org/fhir/us/udap-security/STU1/discovery.html)
+      are tested, verifying the presence of metadata which represents server
+      capabilities. The tested metadata includes all required, optional,
+      recommended, and conditional parameters except for the optional
+      `community` parameter.
+
+      Complete testing of compliance with the FAST Security IG will require
+      testing of
+      [registration](http://hl7.org/fhir/us/udap-security/STU1/registration.html),
+      authorization and authentication of [consumer-facing
+      apps](http://hl7.org/fhir/us/udap-security/STU1/consumer.html)
+      and [B2B
+      apps](http://hl7.org/fhir/us/udap-security/STU1/b2b.html),
+      and [tiered OAuth for user
+      authentication](http://hl7.org/fhir/us/udap-security/STU1/user.html).
+    )
 
     group from: :discovery_group
   end
